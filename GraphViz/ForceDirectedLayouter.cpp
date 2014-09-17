274cf91168383650dbe75ba4872784e219d85365
sefidrodi <sefidrodi@googlemail.com>
2014-09-17 19:42:48 +0200

    added first implementation of forced directed layout procedure, mdeiocre results

--- GraphViz/ForceDirectedLayouter.cpp
+++ GraphViz/ForceDirectedLayouter.cpp
@@ -1,6 +1,7 @@
 #include "ForceDirectedLayouter.h"
 #include <ctime>
 #include <iostream>
+#include "math.h"
 
 SimpleGraph& ForceDirectedLayouter::doLayoutProcedure(int runtimeSeconds){
     time_t startTime = time(NULL);
@@ -10,7 +11,7 @@ SimpleGraph& ForceDirectedLayouter::doLayoutProcedure(int runtimeSeconds){
         doIteration();
         elapsedTime = difftime(time(NULL), startTime);
         double timeSincelastWaitingSignDrawing = difftime(time(NULL), lastWaitingSignDrawing);
-        if (timeSincelastWaitingSignDrawing>0.5){
+        if (timeSincelastWaitingSignDrawing>WAITING_SIGNAL_INTERVALL){
             lastWaitingSignDrawing = time(NULL);
             cout << "*" << endl;
         }
@@ -25,9 +26,34 @@ void ForceDirectedLayouter::doIteration(){
 }
 
 void ForceDirectedLayouter::applyRepulsiveNodeForces(){
-
+    for(std::vector<int>::size_type i = 0; i != graph.nodes.size(); i++) {
+        for(std::vector<int>::size_type j = i+1; j != graph.nodes.size(); j++) {
+            Node& zero = graph.nodes.at(i);
+            Node& one = graph.nodes.at(j);
+            double diffX = one.x - zero.x;
+            double diffY = one.y - zero.y;
+            double repulsiveForce = REPULSION_CONSTANT / std::sqrt(diffY*diffY+diffX*diffX);
+            double theta = atan2(diffY, diffX);
+            zero.x -= repulsiveForce*cos(theta);
+            zero.y -= repulsiveForce*sin(theta);
+            one.x += repulsiveForce*cos(theta);
+            one.y += repulsiveForce*cos(theta);
+        }
+    }
 }
 
 void ForceDirectedLayouter::applyAttractiveEdgeForces(){
-
+    for(std::vector<int>::size_type i = 0; i != graph.edges.size(); i++) {
+        Edge& currentEdge = graph.edges.at(i);
+        Node& zero = graph.nodes[currentEdge.start];
+        Node& one = graph.nodes[currentEdge.end];
+        double diffX = one.x - zero.x;
+        double diffY = one.y - zero.y;
+        double repulsiveForce = ATTRACTION_CONSTANT * (diffY*diffY+diffX*diffX);
+        double theta = atan2(diffY, diffX);
+        zero.x += repulsiveForce*cos(theta);
+        zero.y += repulsiveForce*sin(theta);
+        one.x -= repulsiveForce*cos(theta);
+        one.y -= repulsiveForce*cos(theta);
+    }
 }
--- GraphViz/ForceDirectedLayouter.h
+++ GraphViz/ForceDirectedLayouter.h
@@ -3,6 +3,10 @@
 
 #include "SimpleGraph.h"
 
+#define WAITING_SIGNAL_INTERVALL 0.5
+#define REPULSION_CONSTANT 0.001
+#define ATTRACTION_CONSTANT 0.001
+
 using namespace std;
 class ForceDirectedLayouter {
 public:
